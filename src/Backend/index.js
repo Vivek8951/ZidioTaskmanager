bea886fed2ee6d28765ad68f6327512a3e061bcb
vivek8951 <vivekvic57@gmail.com>
2025-04-18 17:57:22 +0530

    commit

--- src/Backend/index.js
+++ src/Backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import dotenv from 'dotev';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js';
